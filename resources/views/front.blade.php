24b74e2bea57818061a0aba55fba73faca3959a1
rigo mac <mbandrigo@gmail.com>
2025-10-22 09:56:40 +0800

    revisi2

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -76,8 +76,8 @@
                     <div class="card-body text-center">
                         <!-- Logo -->
                         <div>
-                            <img src="{{asset('/logo-pu.jpg')}}" alt="" width="80">
-                            <h2 class="mb-2">SIPEMALUTAJIR</h2>
+                            <img src="{{asset('/logo-pu.jpg')}}" alt="" width="80" class="mb-2">
+                            <h2>SIPEMALUTAJIR</h2>
                         </div>
                         <!-- /Logo -->
                         <h5 class="mb-3">SILAHKAN PILIH SEBAGAI : </h5>
